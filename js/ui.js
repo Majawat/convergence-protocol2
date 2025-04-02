078ccd274d44e61cbba3760b195d82ce7df4e13e
Majawat <12058855+Majawat@users.noreply.github.com>
2025-04-02 17:37:48 -0500

    pull max_spell_tokens from config instead of hardcoded

--- js/ui.js
+++ js/ui.js
@@ -15,7 +15,8 @@ const STAT_ICONS = {
   base: `<i class="bi bi-circle-fill stat-icon" title="Base Size"></i>`,
   tokens: `<svg class="token-icon stat-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16"><title>Tokens</title><path d="M7.657 6.247c.11-.33.576-.33.686 0l.645 1.937a2.89 2.89 0 0 0 1.829 1.828l1.936.645c.33.11.33.576 0 .686l-1.937.645a2.89 2.89 0 0 0-1.828 1.829l-.645 1.936a.361.361 0 0 1-.686 0l-.645-1.937a2.89 2.89 0 0 0-1.828-1.828l-1.937-.645a.361.361 0 0 1 0-.686l1.937-.645a2.89 2.89 0 0 0 1.828-1.828zM3.794 1.148a.217.217 0 0 1 .412 0l.387 1.162c.173.518.579.924 1.097 1.097l1.162.387a.217.217 0 0 1 0 .412l-1.162.387A1.73 1.73 0 0 0 4.58 5.48l-.386 1.161a.217.217 0 0 1-.412 0l-.387-1.162A1.73 1.73 0 0 0 2.806 4.22l-1.162-.387a.217.217 0 0 1 0-.412l1.162-.387A1.73 1.73 0 0 0 3.407 2.31zM10.863.099a.145.145 0 0 1 .274 0l.258.774c.115.346.386.617.732.732l.774.258a.145.145 0 0 1 0 .274l-.774.258a1.16 1.16 0 0 0-.732.732l-.258.774a.145.145 0 0 1-.274 0l-.258-.774a1.16 1.16 0 0 0-.732-.732l-.774-.258a.145.145 0 0 1 0-.274l.774-.258c.346-.115.617-.386.732-.732z"/></svg>`,
 };
-const MAX_SPELL_TOKENS = 6; // Use same constant as app.js
+import { config } from "./config.js"; // Configuration constants
+const MAX_SPELL_TOKENS = config.MAX_SPELL_TOKENS;
 
 /**
  * Creates the HTML for displaying individual models within a unit card.
